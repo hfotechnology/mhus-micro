57502c521127e091ddb3f9b406c0ffbdbf4e9fe5
Mike Hummel <mike.hummel@billingsolutions.de>
2020-07-02 22:53:43 +0200

    save

--- micro-client-docker/src/main/java/de/mhus/micro/docker/DockerProvider.java
+++ micro-client-docker/src/main/java/de/mhus/micro/docker/DockerProvider.java
@@ -2,19 +2,41 @@ package de.mhus.micro.docker;
 
 import java.util.List;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 
 import de.mhus.lib.core.MLog;
 import de.mhus.lib.core.config.IConfig;
 import de.mhus.lib.core.operation.OperationDescription;
 import de.mhus.micro.api.MicroClientProvider;
 
-@Component
+// https://github.com/spotify/docker-client
+
+@Component(immediate = true)
 public class DockerProvider extends MLog implements MicroClientProvider {
 
+    private static DockerProvider instance;
+
+    public static DockerProvider instance() {
+        return instance;
+    }
+
+    
+    @Activate
+    public void doActivate(BundleContext ctx) {
+        log().i("doActivate");
+        instance = this;
+    }
+
+    @Deactivate
+    public void doDeactivate() {
+        instance = null;
+    }
+    
     @Override
     public IConfig execute(String path, IConfig arguments) {
-
         return null;
     }
 
