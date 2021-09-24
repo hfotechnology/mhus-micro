bbca55834433818618e9f4fb83a8f8db1c4a8c2f
Mike Hummel <mike.hummel@billingsolutions.de>
2021-09-24 09:16:22 +0200

    rename operation annotation

--- micro-core/src/test/java/de/mhus/micro/core/test/TestOperation.java
+++ micro-core/src/test/java/de/mhus/micro/core/test/TestOperation.java
@@ -1,12 +1,12 @@
 package de.mhus.micro.core.test;
 
-import de.mhus.lib.annotations.strategy.OperationService;
+import de.mhus.lib.annotations.strategy.OperationDescription;
 import de.mhus.lib.core.operation.AbstractOperation;
 import de.mhus.lib.core.operation.OperationResult;
 import de.mhus.lib.core.operation.Successful;
 import de.mhus.lib.core.operation.TaskContext;
 
-@OperationService(title = "Test Operation")
+@OperationDescription(title = "Test Operation")
 public class TestOperation extends AbstractOperation {
 
 	public boolean done = false;
